750edbad8cd9f2b72db42bb5e5b5b3bfb4f67e52
stephGuill <stephane.guillamo@laplateforme.io>
2025-12-10 11:11:02 +0100

    modification main.jsx, App.jsx et lancer frontend et backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error("Erreur API :", err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Produits</h1>
+
+      {products.length === 0 && <p>Chargement...</p>}
+
+      {products.map(p => (
+        <div key={p.id} style={{
+          border:"1px solid #ddd",
+          padding:"10px",
+          margin:"10px",
+          borderRadius:"8px"
+        }}>
+          <h3>{p.name}</h3>
+          <p>{p.description}</p>
+          <strong>{p.price} €</strong>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default Home;
